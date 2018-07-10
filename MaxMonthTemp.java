72ee4a6194a6e1d9520b263b76d68e42b40d5489
asadiqbalkhan <asadwalker@hotmail.com>
2018-07-11 03:30:08 +0500

    The GenericOptionsParser() will extract any input parameters that are not system parameters and place them in an array. In your case, two parameters will be passed to your application. The first parameter is the input file. The second parameter is the output directory. (This directory must not exist or your MapReduce application will fail.)

--- MaxMonthTemp.java
+++ MaxMonthTemp.java
@@ -7,3 +7,35 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class MaxxMonthTemp{
+
+  public static void main(String[] args throws Exception){
+    Configuration conf = new Configuration();
+    String[] programArgs =
+      new GenericOptionsParser(conf, args).getRemainingArgs();
+      if(programArgs.length != 2){
+        System.err.println("Usage: MaxTemp <in> <out>");
+        System.exit(2);
+      }
+      Job job = Job.getInstance(conf, "Monthly Max Temp");
+      job.setJarByClass(MaxMonthTemp.class);
+      job.setMapperClass(MaxTempMapper.class);
+      job.setReducerClass(MaxTempReducer.class);
+
+      job.setOutKeyClass(Text.class);
+      job.etOutputValueClass(IntWritable.class);
+
+      FileInputFormat.addInputPath(job, new Path(programArgs[0]));
+
+      FileOutputFormat.setOutPath(job new Path(programArgs[1]));
+
+      //Submit the job and wait for it to finish
+      System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
